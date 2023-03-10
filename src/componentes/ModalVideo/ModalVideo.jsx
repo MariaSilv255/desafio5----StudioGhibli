d75feab6ebb67eacab3c4a0ccd8778c73371b3fc
Maria Aparecida <maria.info108@gmail.com>
2023-03-10 13:25:49 -0300

    ajuste finais

--- src/componentes/ModalVideo/ModalVideo.jsx
+++ src/componentes/ModalVideo/ModalVideo.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import Modal from "react-modal";
 import styleV from '../../styles/ModalVideo.module.css'
-
+import { FiX } from "react-icons/fi";
 export default function VideoModal({ openVideo, closeVideo }) {
 
 
     return (
 
+
         <Modal
             className={styleV.stylesCenter}
             isOpen={openVideo}
             onRequestClose={closeVideo}
-       
-
         >
 
-            <iframe width="700" height="350" src="https://www.youtube.com/embed/ByXuk9QqQkk" title={"Spirited Away - Official Trailer"}  allowFullScreen></iframe>
-        </Modal>
+            <iframe width="700" height="350" src="https://www.youtube.com/embed/ByXuk9QqQkk" title={"Spirited Away - Official Trailer"} allowFullScreen></iframe>
 
+        </Modal>
     )
-   
+
 
 }
